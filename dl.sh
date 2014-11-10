d81ca7bc9fa19ee163c8ac979635d5061727aac0
Zhiguang Li <muzili@gmail.com>
2014-11-10 21:44:18 +0800

    Bump version to 1.2
    Signed-off-by: Zhiguang Li <muzili@gmail.com>

--- dl.sh
+++ dl.sh
@@ -5,4 +5,4 @@ mkdir -p dl
 (cd dl && wget http://dl.google.com/android/ndk/android-ndk-r10c-linux-x86_64.bin)
 (cd dl && wget http://mirrors.aliyun.com/apache/ant/binaries/apache-ant-1.9.4-bin.tar.gz)
 (cd dl && wget http://services.gradle.org/distributions/gradle-2.1-bin.zip)
-(cd dl && wget 'http://nj.baidupcs.com/file/d622e6b9aafc3de19ca0e430ccff9880?bkt=p2-nj-542&fid=2197842609-250528-932588678448676&time=1415602264&sign=FDTAXERB-DCb740ccc5511e5e8fedcff06b081203-hlVTzigTj1k7p%2Ftwfh4PizFi%2BJc%3D&to=nb&fm=Nan,B,T,t&newver=1&newfm=1&flow_ver=3&expires=8h&rt=pr&r=646632055&mlogid=1118912&vuk=2197842609&vbdid=1236738697&fin=android-sdk_all-in-one-linux.tgz&fn=android-sdk_all-in-one-linux.tgz')
+(cd dl && wget 'http://bj.baidupcs.com/file/f59b0cf088d88b04f09c46b34b245c50?bkt=p2-nj-542&fid=2197842609-250528-614869431146880&time=1415626948&sign=FDTAXERB-DCb740ccc5511e5e8fedcff06b081203-ndmmW37riDJMgv6txhS%2FyfFStto%3D&to=bb&fm=Nan,B,G,ny&newver=1&newfm=1&flow_ver=3&expires=1415658000&rt=sh&r=233334198&mlogid=1416623736&sh=1&vuk=2197842609&vbdid=1502255842&fin=android-sdk_all-in-one-linux.tgz&fn=android-sdk_all-in-one-linux.tgz' -O android-sdk_all-in-one-linux.tgz)
